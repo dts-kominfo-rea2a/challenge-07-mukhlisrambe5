cf3dab9775c30b784739238e9eae2b91f2830c97
mukhlisrambe5 <mukhlis.rambe5@gmail.com>
2022-06-13 09:13:08 +0700

    feat: challeng-07 first submit

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi)=>{
+  try{
+    const emosiArray= (await promiseTheaterIXX()).concat((await promiseTheaterVGC()))
+    let result = 0;
+    emosiArray.filter(x => x.hasil === emosi && (result += 1) )
+    return result
+  }catch(err) {
+    console.log(err)
+  }
+}
+
 
 module.exports = {
   promiseOutput,
